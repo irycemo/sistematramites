431c73f29edaf84a97d0ff74e2e82d744fbe6ccc
kikorobledo <enrique_j_@hotmail.com>
2023-06-30 11:58:25 -0600

    Fix Tramties

--- app/Http/Livewire/Admin/Tramites.php
+++ app/Http/Livewire/Admin/Tramites.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Log;
 use App\Http\Traits\ComponentesTrait;
 use App\Http\Services\Tramites\TramiteService;
 use App\Http\Services\Tramites\TramitesContext;
+use App\Http\Services\LineasDeCaptura\LineaCaptura;
 
 class Tramites extends Component
 {
@@ -242,6 +243,34 @@ class Tramites extends Component
             $this->modelo_editar = $modelo;
 
         $this->modalVer = true;
+
+    }
+
+    public function validarPago(){
+
+        $array = (new LineaCaptura($this->modelo_editar))->validarLineaDeCaptura();
+
+        if(!isset($array['SOAPBody']['n0MT_ValidarLinCaptura_ECC_Sender']['DOC_PAGO'])){
+
+            $this->dispatchBrowserEvent('mostrarMensaje', ['error', 'No se encontro pago relacionado a la linea de captura.']);
+
+        }else{
+
+            try {
+
+                (new TramiteService($this->modelo_editar))->procesarPago($array['SOAPBody']['n0MT_ValidarLinCaptura_ECC_Sender']['FEC_PAGO'], $array['SOAPBody']['n0MT_ValidarLinCaptura_ECC_Sender']['DOC_PAGO']);
+
+                $this->dispatchBrowserEvent('mostrarMensaje', ['success', "El trámite se validó con éxito."]);
+
+            } catch (\Throwable $th) {
+
+                Log::error("Error al validar trámite por el usuario: (id: " . auth()->user()->id . ") " . auth()->user()->name . ". " . $th);
+                $this->dispatchBrowserEvent('mostrarMensaje', ['error', $th->getMessage()]);
+                $this->resetearTodo($borrado = true);
+
+            }
+
+        }
 
     }
 
